490b53b5312344785f49d7b32805f35941d36ed7
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-04-06 08:32:56 +0900

    ✨ 講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,18 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   const foo = 1;
 
   const handleClick = useCallback((e) => {
@@ -13,6 +22,7 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   }, []);
+
   return (
     <div className={styles.container}>
       <Head>
